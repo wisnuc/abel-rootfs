899e8b4e600c3440bd94cb898e288bbd9f1b9085
matianfu <matianfu@gmail.com>
2018-11-21 14:22:51 +0800

    tmp

--- build.js
+++ build.js
@@ -12,25 +12,11 @@ const mkdirp = require('mkdirp')
 const rimrafAsync = Promise.promisify(rimraf)
 const mkdirpAsync = Promise.promisify(mkdirp)
 
+const exAsync = Promise.promisify(require('./lib/exec'))
+const cog = require('./lib/cog')
+
+const fetch = Promise.promisifyAll(require('./lib/fetch'))
 const partUUID = require('./lib/partUUID')
-const fetch = require('./lib/fetch')
-
-let ubuntuVer = '18.04.1'
-let ubuntuTar = `ubuntu-base-${ubuntuVer}-base-arm64.tar.gz`
-
-let nodeTar
-let nodeVer
-
-// these are set by getKernel function
-// example: 
-// tag: 4.19.0-0000
-// ver: 4.19.0
-// deb: 
-let kernelTag
-let kernelVer
-let kernelDeb
-
-const CONFIG_INSTALL_OVERLAYROOT = false
 
 if (process.getuid()) {
   console.log('this script requires root priviledge')
@@ -40,159 +26,15 @@ if (process.getuid()) {
 let args = process.argv.slice(2)
 args.forEach(arg => {})
 
+const qemus = 'usr/bin/qemu-aarch64-static'
+
 // no root and alt root, only partition p, swap and tmpfs
-const fstab = `
+const fstab = `\
 # <file system>                             <mount point>     <type>  <options>                   <dump>  <fsck>
 UUID=0cbc36fa-3b85-40af-946e-f15dce29d86b   /mnt/persistent   ext4    defaults                    0       1
 UUID=f0bc3049-049f-4e8e-8215-55f48add603f   none              swap    sw                          0       0
-tmpfs                                       /tmp              tmpfs   nodev,nosuid,size=64M       0       0
 tmpfs                                       /var/log          tmpfs   nodev,nosuid,size=64M       0       0
-tmpfs                                       /var/tmp          tmpfs   nodev,nosuid,size=64M       0       0
 `
-
-const getNode = callback => {
-  console.log('retrieving latest node.js lts releases')
-  fetch('https://api.github.com/repos/nodejs/node/releases', (err, body) => {
-    if (err) {
-      callback(err)
-    } else {
-      let vs = body.filter(x => !x.prerelease)
-        .map(x => x.tag_name.split('.'))
-        .map(xs => ({ major: parseInt(xs[0].slice(1)), minor: parseInt(xs[1]), revision: parseInt(xs[2]) }))
-        .sort((a, b) => a.major - b.major ? a.major - b.major : a.minor - b.minor ? a.minor - b.minor : a.revision - b.revision)
-        .reverse()
-
-      let v = vs.find(v => v.major === 10) // LTS version
-      let tag = `v${v.major}.${v.minor}.${v.revision}`
-      let filename = `node-${tag}-linux-arm64.tar.xz`
-      let target = path.join('assets', filename)
-      fs.stat(target, (err, stats) => {
-        if (err && err.code === 'ENOENT') {
-          console.log(`downloading ${filename}`)
-          let href = `https://nodejs.org/dist/${tag}/${filename}`
-          let contentType = 'application/x-xz'
-          let tmpfile = path.join('tmp', filename)
-          fetch({ href, contentType, filename, tmpfile, target }, err => {
-            if (err) {
-              console.log(`download failed, ${err.message}`)
-              callback(err)
-            } else {
-              console.log(`${target} downloaded`)
-              nodeVer = tag
-              nodeTar = target
-              callback(null)
-            }
-          })
-        } else if (err) {
-          callback(err)
-        } else {
-          console.log(`${target} exists, skip download`)
-          nodeVer = tag
-          nodeTar = target
-          callback()
-        }
-      })
-    }
-  })
-}
-
-const getUbuntu = callback => 
-  fs.stat(path.join('assets', ubuntuTar), (err, stats) => {
-    if (err && err.code === 'ENOENT') {
-      console.log(`downloading ${ubuntuTar}`)
-      let filename = ubuntuTar
-      let href = `http://cdimage.ubuntu.com/ubuntu-base/releases/${ubuntuVer}/release/${filename}`
-      let contentType = 'application/x-gzip'
-      let tmpfile = path.join('tmp', filename)
-      let target = path.join('assets', filename)
-      let progress = fetch({ href, contentType, filename, tmpfile, target }, err => {
-        clearInterval(timer)
-        if (err) {
-          console.log(`download failed, ${err.message}`)
-          callback(err)
-        } else {
-          console.log(`download finished`)
-          callback(null)
-        }
-      })
-
-      let timer = setInterval(() => {
-        let p = progress()
-        if (p) {
-          let per
-          if (p.contentLength) {
-            per = new Intl.NumberFormat('en-US', {
-              style: 'decimal',
-              minimumIntegerDigits: 2,
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2
-            }).format(p.bytesWritten * 100/p.contentLength)
-
-            console.log(`${p.bytesWritten}, ${per}%`)
-          } else {
-            console.log(`${p.bytesWritten}`)
-          }
-        }
-      }, 1000)
-    } else if (err) {
-      callback(err)
-    } else {
-      console.log(`${ubuntuTar} exists, skip download`)
-      callback()
-    }
-  })
-
-const getKernel = callback => {
-  console.log('retrieving latest (mainline) kernel package')
-  fetch('https://api.github.com/repos/wisnuc/abel-mainline-kernel/releases', (err, releases) => {
-    if (err) {
-      callback(err)
-    } else {
-      let latest = releases[0]
-      let tag = latest.tag_name
-      let filename = `linux-image-${tag}-arm64.deb`
-      let image = latest.assets.find(x => x.name === filename)
-      if (!image) return callback(new Error('kernel package not found'))
-
-      // TODO skip download if exists
-
-      let href = image.url
-      let contentType = 'application/octet-stream'
-      let tmpfile = path.join('tmp', filename)
-      let target = path.join('assets', filename)
-
-      fs.stat(target, err => {
-        if (err && err.code === 'ENOENT') {
-          fetch({ href, filename, contentType, tmpfile, target }, err => {
-            if (err) {
-              console.log(`download failed, ${err.message}`)
-              callback(err)
-            } else {
-              kernelTag = tag
-              kernelVer = tag.split('-')[0]
-              kernelDeb = filename
-              callback()
-            }
-          })
-        } else if (err) {
-          callback(err)
-        } else {
-          console.log(`${target} exists, skip download`)
-          kernelTag = tag
-          kernelVer = tag.split('-')[0]
-          kernelDeb = filename
-          console.log(kernelTag, kernelVer, kernelDeb)
-          callback(null)
-        }
-      })
-
-    }
-  })
-}
-
-const getUbuntuAsync = Promise.promisify(getUbuntu)
-const getNodeAsync = Promise.promisify(getNode)
-const getKernelAsync = Promise.promisify(getKernel)
 
 const createFile = (file, data, callback) => {
   let rpath = path.join('out', 'rootfs', file) 
@@ -209,6 +51,9 @@ const createFileAsync = Promise.promisify(createFile)
 
 const chrootExec = (cmd, callback) => {
   let args = [ 'out/rootfs', '/bin/bash', '-c', cmd]
+
+  cog(['chroot', ...args].join(' '))
+
   let opts = { stdio: 'inherit' }
   let c = child.spawn('chroot', args, opts)
   c.on('error', err => callback(err))
@@ -252,98 +97,147 @@ wifi.scan-rand-mac-address=no
 `
 
 ;(async () => {
-  await mkdirp('assets')
-  await rimrafAsync('tmp')
-  await mkdirpAsync('tmp')
+  let ubuntu, kernel, node
 
-  await getUbuntuAsync()
-  await getNodeAsync()
-  await getKernelAsync()
+  await exAsync('mkdir -p assets')
+  await exAsync('rm -rf tmp')
+  await exAsync('mkdir -p tmp')
 
-  console.log('all required blobs are ready')
+  ubuntu = await fetch.ubuntuAsync() 
+  node = await fetch.nodeAsync()
+  kernel = await fetch.kernelAsync()
+  ffmpegs = await fetch.ffmpegsAsync()
 
-  await rimrafAsync('out')
-  await mkdirpAsync('out/rootfs')
+  cog('- all required blobs are ready')
 
-  console.log('extracting ubuntu-base')
+  await exAsync(`rm -rf out`)
+  await exAsync(`mkdir -p out/rootfs`)
+  await exAsync(`tar xzf assets/${ubuntu.filename} -C out/rootfs`) 
 
-  await execAsync(`tar xzf assets/${ubuntuTar} -C out/rootfs`) 
+  do {
+    await exAsync(`tar xJf assets/${ffmpegs.filename} -C tmp`)
 
-  await fs.copyFileAsync(path.join('assets', kernelDeb), path.join('out', 'rootfs', kernelDeb))
+    let ms = (await fs.readdirAsync('tmp')).map(name => name.match(/^ffmpeg-git-(\d+)-arm64-static$/)).filter(x => !!x)
+    if (ms.length !== 1) throw new Error('unexpected name for extracted ffmpeg static')
+    let m = ms[0]
+    let dirname = m[0] 
+    ffmpegs.ver = m[1]   
+    ffmpegs.tag = m[1] 
 
-  let qemu = (await execAsync('which qemu-aarch64-static')).trim()
-  await fs.copyFileAsync(qemu, path.join('out', 'rootfs', qemu))
+    await exAsync(`cp tmp/${dirname}/ffmpeg out/rootfs/bin/ffmpeg`)
+    await exAsync(`cp tmp/${dirname}/ffprobe out/rootfs/bin/ffprobe`)
+  } while (0)
+
+  do {
+    await exAsync(`tar xJf assets/${node.filename} --strip-components=1 -C out/rootfs/usr`)
+  } while (0)
+
+  await exAsync(`cp assets/${kernel.filename} out/rootfs`)
+
+  await exAsync(`cp /${qemus} out/rootfs/${qemus}`)
 
   await createFileAsync('etc/resolv.conf', await fs.readFileAsync('/etc/resolv.conf'))
   await createFileAsync('etc/hosts', hosts)
   await createFileAsync('etc/hostname', 'winas\n')
   await createFileAsync('etc/network/interfaces', networkInterfaces)
-  console.log('mounting special file system for chroot')
+  await createFileAsync('etc/timezone', 'Asia/Shanghai\n')
 
-  await execAsync('mount -t proc chproc out/rootfs/proc')
-  await execAsync('mount -t sysfs chsys out/rootfs/sys')
-  await execAsync('mount -t devtmpfs chdev out/rootfs/dev')
-  await execAsync('mount -t devpts chpts out/rootfs/dev/pts')
-  console.log('mounted')
+  cog('mounting special file system for chroot')
+
+  await exAsync('mount -t proc chproc out/rootfs/proc')
+  await exAsync('mount -t sysfs chsys out/rootfs/sys')
+  await exAsync('mount -t devtmpfs chdev out/rootfs/dev')
+  await exAsync('mount -t devpts chpts out/rootfs/dev/pts')
+
+  cog('mounted')
 
   await cexecAsync(`apt update`)
   await cexecAsync(`apt -y upgrade`)
 
   let packages = [
     'initramfs-tools', 
-    // 'ifupdown',
+    'u-boot-tools',
     'net-tools',
     'btrfs-tools',
-    'u-boot-tools',
-    // 'wireless-tools',
+    'tzdata',
+    'wireless-tools',
+    'bluez',
+    'bluetooth',
     'sudo', 
-    'vim',
     'openssh-server',
     'network-manager',
     'avahi-daemon',
     'avahi-utils',
-    'libimage-exiftool-perl',
+    // 'libimage-exiftool-perl',
     'imagemagick',
-    // 'ffmpeg',
     'samba',
     'rsyslog',
     'minidlna',
     'overlayroot'
   ]
 
-  await cexecAsync(`DEBIAN_FRONTEND=noninteractive apt -y install --no-install-recommends ${packages.join(' ')}`)
+  // install packages
+  if (packages.length) {
+    await cexecAsync(`DEBIAN_FRONTEND=noninteractive apt -y install --no-install-recommends ${packages.join(' ')}`)
+  }
+
+  // create first user
   await cexecAsync(`useradd winas -b /home -m -s /bin/bash`)
   await cexecAsync(`echo winas:winas | chpasswd`)
-  await cexecAsync(`adduser winas sudo`)
 
-  await cexecAsync(`dpkg -i ${kernelDeb}`)
-  await cexecAsync(`ln -s vmlinuz-${kernelVer} /boot/Image`)
-  await cexecAsync(`mkimage -A arm64 -O linux -T ramdisk -C gzip -n uInitrd`
-    + ` -d /boot/initrd.img-${kernelVer} /boot/uInitrd-${kernelVer}`)
-  await cexecAsync(`ln -s uInitrd-${kernelVer} /boot/uInitrd`)
-  await cexecAsync(`cp /usr/lib/linux-image-${kernelVer}/rockchip/rk3328-rock64.dtb /boot/rk3328-rock64.dtb`)
+  if (packages.includes('sudo')) {
+    await cexecAsync(`adduser winas sudo`)
+  }
+
+  // install kernel
+  await cexecAsync(`dpkg -i ${kernel.filename}`)
+  await cexecAsync(`ln -s vmlinuz-${kernel.ver} /boot/Image`)
+  if (packages.includes('u-boot-tools')) {
+    await cexecAsync(`mkimage -A arm64 -O linux -T ramdisk -C gzip -n uInitrd -d /boot/initrd.img-${kernel.ver} /boot/uInitrd-${kernel.ver}`)
+    await cexecAsync(`ln -s uInitrd-${kernel.ver} /boot/uInitrd`)
+  }
+  await cexecAsync(`cp /usr/lib/linux-image-${kernel.ver}/rockchip/rk3328-rock64.dtb /boot/rk3328-rock64.dtb`)
   await cexecAsync(`ln -s rk3328-rock64.dtb /boot/dtb`)
+  await cexecAsync(`rm -rf ${kernel.filename}`)
 
-  // dns & network
+  // enable systemd-resolved
   await cexecAsync(`systemctl enable systemd-resolved`)
-//  await cexecAsync(`systemctl enable NetworkManager`)
+  // disable apt daily & motd-news
   await cexecAsync(`systemctl disable apt-daily-upgrade.timer apt-daily.timer motd-news.timer`)
-
+  await cexecAsync(`apt -y remove u-boot-tools initramfs-tools overlayroot`)
+  await cexecAsync(`apt -y autoremove`)
+  await cexecAsync(`apt clean --dry-run`) 
   await cexecAsync(`apt clean`) 
+  await Promise.delay(1000)
 
-  console.log('un-mounting special file system for chroot')
-  await execAsync('umount -l out/rootfs/dev/pts')
-  await execAsync('umount -l out/rootfs/dev')
-  await execAsync('umount -l out/rootfs/sys')
-  await execAsync('umount -l out/rootfs/proc')
-  console.log('un-mounted')
+  // await cexecAsync(`apt -y --allow-remove-essential remove apt`)
 
-  await createFileAsync('etc/NetworkManager/NetworkManager.conf', nmconf)
-  await createFileAsync('etc/NetworkManager/conf.d/10-globally-managed-devices.conf', '')
-  await rimrafAsync(path.join('out', 'rootfs', 'etc/resolv.conf'))
-  await execAsync(`ln -sf /run/systemd/resolve/resolv.conf out/rootfs/etc/resolv.conf`)
-  await rimrafAsync(path.join('out', 'rootfs', kernelDeb)) 
+  cog('un-mounting special file system for chroot')
 
+  await exAsync('umount -l out/rootfs/dev/pts')
+  await exAsync('umount -l out/rootfs/dev')
+  await exAsync('umount -l out/rootfs/sys')
+  await exAsync('umount -l out/rootfs/proc')
+
+  cog('un-mounted')
+
+  await exAsync(`rm -rf out/rootfs/var/lib/apt/lists`)
+  await exAsync(`mkdir -p out/rootfs/var/lib/apt/lists`)
+
+  // remove qemu static
+  await exAsync(`rm out/rootfs/${qemus}`)
+  // systemd-resolved
+  // await rimrafAsync(path.join('out', 'rootfs', 'etc/resolv.conf'))
+  await exAsync('rm out/rootfs/etc/resolv.conf') 
+  await exAsync(`ln -sf /run/systemd/resolve/resolv.conf out/rootfs/etc/resolv.conf`)
+  // network manager
+  if (packages.includes('network-manager')) {
+    await createFileAsync('etc/NetworkManager/NetworkManager.conf', nmconf)
+    await createFileAsync('etc/NetworkManager/conf.d/10-globally-managed-devices.conf', '')
+  }
+  // fstab
+  await createFileAsync('etc/fstab', fstab)
+  // overlayroot
   await createFileAsync('etc/overlayroot.conf', 'overlayroot="tmpfs:swap=1,recurse=0"')
 
   await mkdirpAsync('out/rootfs/tmp')
@@ -352,19 +246,22 @@ wifi.scan-rand-mac-address=no
   await mkdirpAsync('out/rootfs/mnt/persistent')
   await mkdirpAsync('out/rootfs/winas')
   
-  await createFileAsync('etc/fstab', fstab)
+  await exAsync('mkdir -p out/p/boot')
+  await exAsync('cp assets/boot.cmd out/p/boot/boot.cmd')
+  await exAsync(`mkimage -C none -A arm -T script -d out/p/boot/boot.cmd out/p/boot/boot.scr`)
 
-  await mkdirpAsync('out/p/boot')
-  await fs.copyFileAsync('assets/boot.cmd', 'out/p/boot/boot.cmd')
-  await execAsync(`mkimage -C none -A arm -T script -d out/p/boot/boot.cmd out/p/boot/boot.scr`)
+  await exAsync('mkdir -p out/p/overlay/etc')
 
-  await mkdirpAsync('out/p/overlay/etc')
-  await execAsync('mv out/rootfs/etc/NetworkManager out/p/overlay/etc')
-  await execAsync('ln -s /mnt/persistent/overlay/etc/NetworkManager out/rootfs/etc/NetworkManager')
+  if (packages.includes('network-manager')) {
+    await exAsync('mv out/rootfs/etc/NetworkManager out/p/overlay/etc')
+    await exAsync('ln -s /mnt/persistent/overlay/etc/NetworkManager out/rootfs/etc/NetworkManager')
+  }
 
-  await execAsync(`tar czf out/rootfs.tar.gz -C out/rootfs .`)
-  console.log('rootfs.tar.gz is ready')
+  await exAsync(`tar czf out/rootfs.tar.gz -C out/rootfs .`)
 
-  await execAsync(`tar czf out/p.tar.gz -C out/p .`)
-  console.log('p.tar.gz is ready')
+  cog('rootfs.tar.gz is ready')
+
+  await exAsync(`tar czf out/p.tar.gz -C out/p .`)
+
+  cog('p.tar.gz is ready')
 })().then(() => {}).catch(e => console.log(e))
